74dcec9a515459a4b5a6988842b47168ab3f473f
klesh22 <karensiegel22@gmail.com>
2015-03-25 22:34:28 +0000

    sending

--- 04-strings/00-basics/challenge.js
+++ 04-strings/00-basics/challenge.js
@@ -1,19 +1,37 @@
-module.exports.formLetter = function(firstName, senderName, message) {
-
+var formLetter = function(firstName, senderName, message) {
+return formLetter="Hello John,\n\nI think I have this figured out now.\n\nSincerely, Karen"
 };
 
-module.exports.sliceItAndCombineIt = function(bigString, startA, endA, startB, endB) {
-
+module.exports.sliceItAndCombineIt = function(bigString, startA, endA, startB, endB) { 
+var bigString = "This is a long string.";
+var subStringA = bigString.substring(0,5);
+var subStringA;
+  subStringA= bigString.substring(startA,endA);
+  var subStringB = bigString.substring(7,9);
+  var subStringB;
+  subStringB = bigString.substring(startB, endB);
+  return subStringA + subStringB;
 };
 
+var text = "John is my mentor.";
+var searchString = "my ";
 module.exports.findFirstMatch = function(text, searchString) {
-
+  return text.IndexOf(searchTerm);
 };
+findFirstMatch(text,searchString)
 
+var text = "John is my mentor.";
+var searchString = "my";
 module.exports.findLastMatch = function(text, searchString) {
+ return text.lastIndexOf(searchTerm);
+};
+findLastMatch(text,searchString)
 
+var text = "roses are red violets are blue";
+var searchString = "violets";
+module.exports.substringBetweenMatches = function(text, searchString) {
+  return text.substring(14,21);
 };
 
-module.exports.substringBetweenMatches = function(text, searchString) {
 
 };
