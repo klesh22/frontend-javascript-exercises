c80c854f6db63269f5b22e6cac5837997d89684d
klesh22 <karensiegel22@gmail.com>
2015-03-26 00:49:06 +0000

    finish last two exercises

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -1,14 +1,27 @@
-module.exports.equalStrings = function(stringOne, stringTwo) {
-
+var stringOne = "one";
+var stringTwo = "two";
+var equalStrings = function(stringOne, stringTwo) {
+  return (stringOne == stringTwo);
 };
+equalStrings(stringOne,stringTwo);
+John, do I need to change the "module exports to 'var'"????
 
-module.exports.notEqual = function(one, two) {
 
+ var one = 1
+ var two = 2
+var notEqual = function(one, two) {
+  return (one != two);
 };
+notEqual(one,two);
 
-module.exports.inBetween = function(lower, middle, upper) {
+var lower = 1;
+var middle = 9;
+var upper = 3;
+var inBetween = function(lower, middle, upper) {
+  return (lower<middle && middle<upper);
+ };
+inBetween(lower,middle,upper); 
 
-};
 
 module.exports.outsideRanges = function(number) {
 
