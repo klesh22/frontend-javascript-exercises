da86091121004d6e8576e341d100392d9e44873c
Karen Siegel <karensiegel22@gmail.com>
2015-05-06 19:35:21 +0000

    loops

--- 10-loops/00-looping-over-arrays/challenge.js
+++ 10-loops/00-looping-over-arrays/challenge.js
@@ -1,5 +1,65 @@
-module.exports.sumNumbers = undefined;
+/*
+- Complete the `sumNumbers` function. This function should:
+ - return the sum of all numbers inside of the array that is passed to the function
+    - if the array is empty, this function should return 0
 
-module.exports.splitAndLowerCaseString = undefined;
+    For example:
+    
+    ```javascript
+    sumNumbers([1, 2, 3]) // should return 6
+    ```
+*/
+module.exports.sumNumbers = function(array) {
+
+var array = [1, 2, 3];
+
+for (var i = 0, sum = 0; i < array.length; sum += array[i++]) {
+    console.log(sum);
+};
+    
+
+
+
+
+
+/*
+- Complete the `splitAndLowerCaseString` function. This function should:
+    - split the `inputString` argument into an array of strings by commas
+    - return an array with the set of split strings in lowercase format
+
+    For example:
+    
+    ```javascript
+    splitAndLowerCaseString("TEST") // returns ["test"]
+    splitAndLowerCaseString("THIS,IS,SPLIT,BY,COMMAS") // returns ["this", "is", "split", "by", "commas"]
+    splitAndLowerCaseString("THIS HAS NO COMMAS") // returns ["this has no commas"]
+    ```
+
+    > Hint: You should use the built-in string functions `split` and `toLowerCase`.
+*/
+
+module.exports.splitAndLowerCaseString = function (array) {
+    var finalArray= array.split(",");
+    return array.toLowerCase;
+    };
+    console.log.splitAndLowerCaseString(array);
+
+
+
+
+/*
+- Complete the `addIndex` function. This function should:
+    - create and return a new array of all inputs as strings
+    - each of the new strings should start with "[index] is [data element at index]"
+
+    For example:
+    
+    ```javascript
+    addIndex([1, 2, 3]) // returns ["0 is 1", "1 is 2", "2 is 3"]
+    addIndex(['My', 1, 'number']) // returns ["0 is My", "1 is 1", "2 is number"]
+    ```
+
+*/
+
 
 module.exports.addIndex = undefined;
